a6068ffeebfdd33dde62b38e00ef84db6851eb30
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2022-11-25 00:17:44 +0800

    Added personal dmenu configuration

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Iosevka Fixed Slab:size=10:style=Medium Extended"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#ffffff", "#282a36" },
+    [SchemeSel] = { "#ffffff", "#8a8ea8" },
+    [SchemeOut] = { "#ffffff", "#757575" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
