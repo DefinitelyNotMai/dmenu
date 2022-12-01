cfe6265fe66b25e6a654b19aeadaec1595118288
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2022-12-01 21:34:12 +0800

    Reduced font size to 8

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "Iosevka Fixed Slab:size=10:style=Medium Extended"
+    "Iosevka Fixed Slab:size=8:style=Medium Extended"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
